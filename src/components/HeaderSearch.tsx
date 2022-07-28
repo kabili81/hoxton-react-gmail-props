85e034f9b85f5474001f89b8aa00604c5025c590
Kabil Ibrahimi <kabili81@hotmail.com>
2022-07-28 10:08:47 +0200

    updeta

---
+++ src/components/HeaderSearch.tsx
@@ -0,0 +1,8 @@
+export function HeaderSearch() {
+    return(
+        
+        <div className="search">
+          <input className="search-bar" placeholder="Search mail" />
+        </div>
+    )
+}
